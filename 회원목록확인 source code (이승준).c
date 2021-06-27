6d4d9b49d75107e129d7b8a0e7c288cec096b1ee
lsjs0706 <80740583+lsjs0706@users.noreply.github.com>
2021-06-28 02:33:05 +0900

    Create 회원목록확인 source code (이승준).c
    회원목록확인 기능 추가

---
+++ 회원목록확인 source code (이승준).c
@@ -0,0 +1,34 @@
+void list(struct student* info)
+{
+    int i, j;
+    printf("1. 전체 회원정보 조회 2. 개인 회원정보 조회\n");
+    scanf("%d", &i);
+    struct student m[100];
+    FILE* fp;
+    fp = fopen("exampleFile.txt", "r");
+    char tmp;
+    int cnt = 1;
+    if (fp == NULL) {
+        exit(0);
+    }
+    while (fscanf(fp, "%c", &tmp) != EOF) {
+        printf("%c", tmp);
+        if (tmp == '\n')
+            cnt++;
+    }
+    printf("\n\n라인의 개수=%d\n", cnt);
+    return 0;
+    for (int j = 0; j < cnt; j++)
+    {
+        fscanf(fp, "%s %s %s %s %s\n", &m[j].name, &m[j].ID, &m[j].call, &m[j].mail, &m[j].password);
+        fclose(fp);
+    }
+    
+    if (i == 1)
+    {
+        for (int j = 0; j < cnt; j++)
+        {
+            printf("이름:%s 학번:%s 전화번호:%s 이메일:%s 비밀번호:%s\n", &m[j].name, &m[j].ID, &m[j].call, &m[j].mail, &m[j].password);
+        }
+    }
+}

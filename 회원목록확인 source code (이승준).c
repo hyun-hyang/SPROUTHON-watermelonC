49682d736c601bbbd97c5a320e0d2078ef148bba
lsjs0706 <80740583+lsjs0706@users.noreply.github.com>
2021-06-29 13:21:07 +0900

    Update 회원목록확인 source code (이승준).c
    파일 읽어서 구조체에 저장 (완료)

--- 회원목록확인 source code (이승준).c
+++ 회원목록확인 source code (이승준).c
@@ -1,9 +1,9 @@
-void list(struct student* info)
+void list(struct student* info, struct student* m)
 {
     int i, j;
     printf("전체 회원정보를 조회하시겠습니까? 1.Yes 2. No\n");
     scanf("%d", &i);
-    
+
     FILE* fp;
     fp = fopen("exampleFile.txt", "r");
     char tmp;
@@ -15,12 +15,20 @@ void list(struct student* info)
     {
         while (fscanf(fp, "%c", &tmp) != EOF)
         {
-            printf("%c", tmp);
             if (tmp == '\n')
                 cnt++;
         }
         printf("회원 수는 %d명입니다.\n", cnt);
-        printf("\n");
+
+        fp = fopen("exampleFile.txt", "r");
+        for (int k = 0; k < cnt; k++)
+        {
+            
+            fscanf(fp, "%s %s %s %s %s\n", &m[k].name, &m[k].ID, &m[k].call, &m[k].mail, &m[k].password);
+            printf("이름:%s 학번:%s 전화번호:%s 이메일:%s 비밀번호:%s\n", m[k].name, m[k].ID, m[k].call, m[k].mail, m[k].password);
+
+        }
+        fclose(fp);
     }
     else
         exit(0);

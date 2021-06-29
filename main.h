4bfffbe16b81109dca783bab86c4a632dc33499f
jimin3060 <51802020+jimin3060@users.noreply.github.com>
2021-06-29 17:28:28 +0900

    Add files via upload

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef __STDIO_H__
+#define __STDIO_H__
+
+#include <stdio.h>
+
+#endif
+
+#ifndef __STRING_H__
+#define __STRING_H__
+
+#include <string.h>
+
+#endif
+
+#define CRT_SECURE_NO_WARNINGS
+
+#pragma warning(disable : 4996)

ec7ca88d61c31f8aed863e96621be263a92a62b2
jimin3060 <51802020+jimin3060@users.noreply.github.com>
2021-06-29 17:29:08 +0900

    Add files via upload

---
+++ calenderprint.h
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int calenderprint(void);
+int get_day(int yyyy, int mm);
+int get_day_of_month(int yyyy, int mm);
+int is_leaf_year(int yyyy);
+void print_cal(int start_day, int day_num);
